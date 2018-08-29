86926472065379507144739be1cc91ed810b9e16
tzuyi00 <tzuyi00@gmail.com>
2018-08-29 19:06:17 +0800

    抓資料

--- js/expert.js
+++ js/expert.js
@@ -70,116 +70,94 @@ $(function () {
 
 
 //=====愛心換圖=====
-// $("#heart").click(function () {
-// 	if (heart.title === "加入收藏") {
-// 		$(this).attr("src", "../img/expertImg/heartRed.png");
-// 		$(this).attr("title", "取消收藏");
-// 	} else {
-// 		$(this).attr("src", "../img/expertImg/heartWhite.png");
-// 		$(this).attr("title", "加入收藏");
-// 	}
-// })
-
-
-
-
-
-
-// =======抓資料庫=======
-$(function () {
-	var grid = document.querySelector('.grid');
-	var xhr = new XMLHttpRequest();
-	var url = "php/expert.php";
-	xhr.open("Get", url, true);
-	xhr.send(null);
-	xhr.onload = function () {
-		if (xhr.status == 200) {
-			console.log(xhr.responseText);
-			var memRow = JSON.parse(xhr.responseText);
-			// memRow.forEach(e => {
-			// 	grid.innerHTML += `<div class="element-item">
-			// 	<img class="king" src="img/expertImg/crown.png" alt="crown">
-			//     <h2 class="h2Desk">${e.planet}</h2>
-			//     <h3 class="h3Desk">${e.expertName}</h3>
-			//     <div class="attr">美食</div>
-			//     <div class="pic">
-			//         <a>
-			//             <img id="box" src="../img/expertImg/expPic/101.jpg">
-			//         </a>
-			//         <div class="aside">
-			//             <h2 class="h2Ph">瓦特星</h2>
-			//             <h3 class="h3Ph">Christina</h3>
-			//             <div class="score">
-			//                 <span>5</span>
-			//                 <img src="img/expertImg/star.png" alt="star">
-			//                 <img src="img/expertImg/star.png" alt="star">
-			//                 <img src="img/expertImg/star.png" alt="star">
-			//                 <img src="img/expertImg/star.png" alt="star">
-			//                 <img src="img/expertImg/star.png" alt="star">
-			//             </div>
-			//             <div class="mark">
-			//                 <img src="img/expertImg/comment.png" alt="comment">
-			//                 <span>5</span>
-			//                 <img src="img/expertImg/love.png" alt="love">
-			//                 <span>20</span>
-			//             </div>
-			//         </div>
-			//     </div>
-			// </div>`;
-			// });
-			// $('.element-item h2').each(function () {
-			// 	if ($(this).html() == '瓦特星') {
-			// 		$(this).parent().addClass('blue');
-			// 	}
-			// 	else if ($(this).html() == '達沙星') {
-			// 		$(this).parent().addClass('orange');
-			// 	}
-			// 	else {
-			// 		$(this).parent().addClass('green');
-			// 	}
-
-			// if ($(this).data("pop") > 20) {
-			// 	$(this).parent().addClass('popular');
-			// }
-
-		} else {
-			alert(xhr.status);
-		}
+$("#heart").click(function () {
+	if (heart.title === "加入收藏") {
+		$(this).attr("src", "../img/expertImg/heartRed.png");
+		$(this).attr("title", "取消收藏");
+	} else {
+		$(this).attr("src", "../img/expertImg/heartWhite.png");
+		$(this).attr("title", "加入收藏");
 	}
-
-
 })
+
+
+
+
+
+
 // =======抓資料庫=======
-
-
-// var memRow = JSON.parse(xhr.responseText);
+// $(function () {
+// 	var grid = document.querySelector('.grid');
+// 	var xhr = new XMLHttpRequest();
+// 	var url = "php/expert.php";
+// 	xhr.open("Get", url, true);
+// 	xhr.send(null);
+// 	xhr.onload = function () {
+// 		if (xhr.status == 200) {
+// 			console.log(xhr.responseText);
+// 			var memRow = JSON.parse(xhr.responseText);
 // memRow.forEach(e => {
 // 	grid.innerHTML += `<div class="element-item">
-// 				<img class="king" src="img/expertImg/crown.png" alt="crown">
-//                 <h2 class="h2Desk">${e.planet}</h2>
-//                 <h3 class="h3Desk">${e.expertName}</h3>
-//                 <div class="attr">美食</div>
-//                 <div class="pic">
-//                     <a>
-//                         <img id="box" src="../img/expertImg/expPic/101.jpg">
-//                     </a>
-//                     <div class="aside">
-//                         <h2 class="h2Ph">瓦特星</h2>
-//                         <h3 class="h3Ph">Christina</h3>
-//                         <div class="score">
-//                             <span>5</span>
-//                             <img src="img/expertImg/star.png" alt="star">
-//                             <img src="img/expertImg/star.png" alt="star">
-//                             <img src="img/expertImg/star.png" alt="star">
-//                             <img src="img/expertImg/star.png" alt="star">
-//                             <img src="img/expertImg/star.png" alt="star">
-//                         </div>
-//                         <div class="mark">
-//                             <img src="img/expertImg/comment.png" alt="comment">
-//                             <span>5</span>
-//                             <img src="img/expertImg/love.png" alt="love">
-//                             <span>20</span>
-//                         </div>
-//                     </div>
-//                 </div>
-// 			</div>`;
+// 	<img class="king" src="img/expertImg/crown.png" alt="crown">
+//     <h2 class="h2Desk">${e.planet}</h2>
+//     <h3 class="h3Desk">${e.expertName}</h3>
+//     <div class="attr">美食</div>
+//     <div class="pic">
+//         <a>
+//             <img id="box" src="../img/expertImg/expPic/101.jpg">
+//         </a>
+//         <div class="aside">
+//             <h2 class="h2Ph">瓦特星</h2>
+//             <h3 class="h3Ph">Christina</h3>
+//             <div class="score">
+//                 <span>5</span>
+//                 <img src="img/expertImg/star.png" alt="star">
+//                 <img src="img/expertImg/star.png" alt="star">
+//                 <img src="img/expertImg/star.png" alt="star">
+//                 <img src="img/expertImg/star.png" alt="star">
+//                 <img src="img/expertImg/star.png" alt="star">
+//             </div>
+//             <div class="mark">
+//                 <img src="img/expertImg/comment.png" alt="comment">
+//                 <span>5</span>
+//                 <img src="img/expertImg/love.png" alt="love">
+//                 <span>20</span>
+//             </div>
+//         </div>
+//     </div>
+// </div>`;
+// });
+// $('.element-item h2').each(function () {
+// 	if ($(this).html() == '瓦特星') {
+// 		$(this).parent().addClass('blue');
+// 	}
+// 	else if ($(this).html() == '達沙星') {
+// 		$(this).parent().addClass('orange');
+// 	}
+// 	else {
+// 		$(this).parent().addClass('green');
+// 	}
+
+// if ($(this).data("pop") > 20) {
+// 	$(this).parent().addClass('popular');
+// }
+
+// 		} else {
+// 			alert(xhr.status);
+// 		}
+// 	}
+// })
+// =======抓資料庫=======
+
+$.ajax({
+	url: 'php/expert.php',
+	dataType: 'text',
+	success: function (data) {
+		$('.element-item.expertBox.blue.popular').append(data);
+		alert('success');
+		console.log(1);
+	},
+	error: function () {
+		alert('error');
+	}
+});
--- php/expert.php
+++ php/expert.php
@@ -3,10 +3,18 @@ try {
     require_once("connectExpert.php");
     $sql = "select * from expert";
     $members = $pdo->query($sql);
-    $memRow = $members->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($memRow);
+    $memRows = $members->fetchAll(PDO::FETCH_ASSOC);
+    // echo json_encode($memRow);
+    foreach ($memRows as $memRow) {
+    	echo '<h2 class ="h2Desk">', $memRow['planet'], '</h2>',
+			 '<h3 class ="h3Desk">', $memRow['expertName'], '</h3>';
+			
 
+        echo '<h2 class ="h2Desk">', $memRow['planet'], '</h2>';
+        echo '<h3 class ="h3Desk">', $memRow['expertName'], '</h3>';
+    }
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo "錯誤原因 : ", $e->getMessage(), "<br>";
+    echo "錯誤行號 : ", $e->getLine(), "<br>";
 }
 ?>
