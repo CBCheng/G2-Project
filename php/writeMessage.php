280f3470dd52277a8c26328dfd6a062f6e9ba362
poaAlways168 <bombs@livemail.tw>
2018-09-07 17:59:14 +0800

    refer ok

--- php/writeMessage.php
+++ php/writeMessage.php
@@ -2,9 +2,15 @@
 
 $content = htmlspecialchars($_REQUEST['content']);
 $scheduleNo = $_REQUEST['scheduleNo'];
-$memNo = $_REQUEST['memNo'];
+$memName = $_REQUEST['memName'];
 try {
 	require_once("connectCd102g2.php");
+    //先抓到會員編號
+    $sqlName = "select * from member where MEM_NAME='$memName'";
+    $catchName = $pdo->query($sqlName);
+    $catchNameRow = $catchName->fetch(PDO::FETCH_ASSOC);
+    $memNo = $catchNameRow['MEM_NO'];
+
     //把留言寫入資料庫
 	$sql = "insert into itineraryre values (null, :scheduleNo, :memNo, now(), :content, 0)";
 	$comment = $pdo->prepare($sql);
@@ -18,14 +24,14 @@ try {
     $message = $pdo->exec($sqlMessage);
 
     //把留言資料傳回頁面
-	$sqlAdd = "select * from itineraryre where scheduleNo=$scheduleNo ORDER BY itineraryre.commentNo ASC";
+	$sqlAdd = "select * from cd102g2.itineraryre as a join cd102g2.member as b on a.memNo = b.MEM_NO where scheduleNo=$scheduleNo ORDER BY a.commentNo ASC";
 	$itineraryre = $pdo->query($sqlAdd);
 	$itiRow = $itineraryre->fetchAll(PDO::FETCH_ASSOC);
 	foreach ($itiRow as $data) {
-        //抓會員編號的SQL
-		$sqlMem = "select * from cd102g2.itineraryre as a join cd102g2.member as b on a.memNo = b.MEM_NO";
-        $memName = $pdo->query($sqlMem);
-        $memRow = $memName->fetch(PDO::FETCH_ASSOC);
+  //       //抓會員編號的SQL
+		// $sqlMem = "select * from cd102g2.itineraryre as a join cd102g2.member as b on a.memNo = b.MEM_NO";
+  //       $memName = $pdo->query($sqlMem);
+  //       $memRow = $memName->fetch(PDO::FETCH_ASSOC);
         //抓行程編號留言數SQL
         $sqlCom = "select * from myschedule where scheduleNo=$scheduleNo";
 		$com = $pdo->query($sqlCom);
@@ -33,7 +39,7 @@ try {
 ?>
 		<div class="commentWrip">
                     <div class="commentPic">
-                        <img src="img/member_pic.jpg" alt="">
+                        <img src="img/member/<?php echo $data['MEM_IMG']?>" alt="">
                     </div>
                     
                     <div class="commentContent">
@@ -41,7 +47,7 @@ try {
                             <input type="hidden" id="commTime" value="<?php echo $comRow['messageNum']?>">
                             <input type="hidden" name="reportTime" value="<?php echo $data['commentNo']?>">
                             <p class="deta"><?php echo $data['commentTime']?></p>
-                            <p class="memName"><?php echo $memRow['MEM_NAME']?></p>
+                            <p class="memName"><?php echo $data['MEM_NAME']?></p>
                             <p class="commentTxt"><?php echo $data['reContent']?></p>
                             <button class="reportBtn">檢舉</button>
                         </div>
@@ -57,8 +63,8 @@ try {
 
 	
 } catch (PDOException $e) {
-	echo "error~<br>";
-  	echo $e->getMessage() , "<br>";
+	echo "錯誤原因 : " , $e->getMessage(), "<br>";
+    echo "錯誤行號 : " , $e->getLine(), "<br>"; 
 }
 ?>
 <script type="text/javascript">
